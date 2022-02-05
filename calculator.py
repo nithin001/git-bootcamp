46e0cd8b99e20af8cb7f2e1a3c1c6209810bef93
nithish <nithishkumar.appscomp@gmail.com>
2022-02-05 11:57:48 +0530

    implemented multiplication

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
 def calc_sum(a, b):
-    return a+b
+    return a + b
 
+
+def calc_multiple(a, b):
+    return a * b * b
--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,14 @@
 import unittest
-from calculator import calc_sum
+from calculator import calc_sum, calc_multiple
 
 
 class Test(unittest.TestCase):
     def test_calc_sum(self):
         self.assertEqual(calc_sum(1, 2), 3, "Should be 3")
+
+    def test_multiple(self):
+        self.assertEqual(calc_multiple(3, 4), 12, "Should be 12")
+        self.assertEqual(calc_multiple(5, 6), 30, "Should be 30")
 
 
 if __name__ == '__main__':
